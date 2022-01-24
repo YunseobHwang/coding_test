6344133b045cf7fdf1938d7239f80fe8394767bc
YunseobHwang <yunseob1102@gmail.com>
2022-01-24 21:01:17 +0900

    Add a new problem-solving code

---
+++ programmers/코딩테스트연습_Greedy_큰 수 만들기_argparse사용해봄.py
@@ -0,0 +1,26 @@
+
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--number', action='store', type=str, default='0', help='given number')
+    parser.add_argument('--k', action='store', type=int, default=0, help='elimination number')
+    args = parser.parse_args()
+    print(f'args={args}\n')
+    return args
+
+def solution(number, k):
+    answer = ''
+    for n in number:
+        while len(answer) > 0 and n > answer[-1] and k > 0:
+            answer = answer[:-1]
+            k -= 1
+        answer += n
+
+    if k > 0:
+        answer = answer[:-k]
+    return answer
+
+if __name__=='__main__':
+    args = parse_arguments()
+    solution(args.number, args.k)
